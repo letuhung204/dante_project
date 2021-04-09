212bd0f70cc33dcd45124397de063001e08194e8
letuhung204 <tuhung204@gmail.com>
2021-04-10 00:59:22 +0700

    add config

--- src/main/java/com/dante/auth/service/UserDetailsServiceImpl.java
+++ src/main/java/com/dante/auth/service/UserDetailsServiceImpl.java
@@ -27,10 +27,7 @@ public class UserDetailsServiceImpl implements UserDetailsService{
         if (user == null) throw new UsernameNotFoundException(username);
 
         Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
-        for (Role role : user.getRoles()){
-            grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
-        }
 
-        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
+        return null;
     }
 }
